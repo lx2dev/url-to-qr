0199ce4c344c7271399f262c7972f17076fbb872
SlickYeet <lasse@famlam.ca>
2026-01-30 23:57:08 -0800

    feat: implement authentication and QR code generation pages

--- src/app/qrcodes/page.tsx
+++ src/app/qrcodes/page.tsx
@@ -3,11 +3,18 @@ import { redirect } from "next/navigation"
 
 import { auth } from "@/server/auth"
 
-export default async function HomePage() {
+export default async function QRCodesPage() {
   const session = await auth.api.getSession({
     headers: await headers(),
   })
 
   if (!session) return redirect("/auth")
-  else return redirect("/qrcodes")
+
+  return (
+    <div>
+      <h1>QRCodesPage</h1>
+
+      <p>Welcome, {session.user.name}!</p>
+    </div>
+  )
 }
