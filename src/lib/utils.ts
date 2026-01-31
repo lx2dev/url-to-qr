aaf62366d0eddbd8fee137fc8a12ece34929f36d
SlickYeet <lasse@famlam.ca>
2026-01-30 21:23:20 -0800

    feat: init shadcn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,12 @@
+import type { ClassValue } from "clsx"
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 import { env } from "@/env"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin
