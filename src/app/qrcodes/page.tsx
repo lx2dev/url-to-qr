0347b09e3729a709d91bef056bce467d5c7f65cc
SlickYeet <lasse@famlam.ca>
2026-01-31 03:23:34 -0800

    feat: enhance QRCodesPage layout with header, form, and list components

--- src/app/qrcodes/page.tsx
+++ src/app/qrcodes/page.tsx
@@ -1,6 +1,12 @@
 import { headers } from "next/headers"
 import { redirect } from "next/navigation"
 
+import { CreateQRCodeForm } from "@/components/qrcodes/create-form"
+import { QRCodesHeader } from "@/components/qrcodes/header"
+import { QRCodesList } from "@/components/qrcodes/qr-codes-list"
+import { Card } from "@/components/ui/card"
+import { DEFAULT_FETCH_LIMIT } from "@/constants"
+import { api, HydrateClient } from "@/lib/api/server"
 import { auth } from "@/server/auth"
 
 export default async function QRCodesPage() {
@@ -10,11 +16,23 @@ export default async function QRCodesPage() {
 
   if (!session) return redirect("/auth")
 
-  return (
-    <div>
-      <h1>QRCodesPage</h1>
+  void api.qrCode.list.prefetchInfinite({
+    limit: DEFAULT_FETCH_LIMIT,
+  })
 
-      <p>Welcome, {session.user.name}!</p>
-    </div>
+  return (
+    <HydrateClient>
+      <div className="min-h-screen">
+        <div className="container mx-auto max-w-2xl px-4 py-6 sm:py-8">
+          <QRCodesHeader />
+
+          <Card className="mb-8 p-6 dark:bg-card/50 dark:backdrop-blur">
+            <CreateQRCodeForm />
+          </Card>
+
+          <QRCodesList />
+        </div>
+      </div>
+    </HydrateClient>
   )
 }
